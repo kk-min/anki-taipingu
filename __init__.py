6c29250761bf1807b4b99e7e0c5b8962143a06d3
kk-min <76023265+kk-min@users.noreply.github.com>
2024-01-25 10:12:07 +0800

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,51 @@
+from aqt import mw, gui_hooks
+from aqt.utils import showInfo, showCritical
+from aqt.qt import *
+
+class TypingDialog(QDialog):
+    def __init__(self, parent, title, ans):
+        super().__init__(parent)
+        self.setWindowTitle("Taipingu")
+        self.setModal(False)
+        self.layout = QVBoxLayout(self)
+        self.ans = ans
+
+        self.label = QLabel(self)
+        self.label.setText(ans)
+        self.label.move(200, 100)
+        self.label.setStyleSheet("font-size: 24px; text-align: center; width: 300px;")
+        self.layout.addWidget(self.label)
+
+        self.lineedit = QLineEdit(self)
+        self.label.move(200, 200)
+        self.lineedit.setStyleSheet("font-size: 24px; width: 300px;")
+        self.layout.addWidget(self.lineedit)
+
+        self.b1 = QPushButton("Ok", self)
+        self.label.move(200, 300)
+        self.b1.setStyleSheet("font-size: 24px; width: 300px;")
+        self.b1.clicked.connect(self.check)
+        self.layout.addWidget(self.b1)
+
+        self.show()
+
+    def check(self):
+        userInput = self.lineedit.text()
+        if userInput == self.ans:
+            showInfo("Correct!")
+        else:
+            showCritical("Incorrect!")
+
+    def exit(self):
+        self.close()
+
+def typingTest(card):
+    keyList = ['Expression', 'Key', 'Spelling']
+    ans = "N/A"
+    for key in keyList:
+        if key in card.note():
+            ans = card.note()[key]
+            break
+    mw.typeWidget = TypingDialog(mw, "Taipingu", ans)
+
+gui_hooks.reviewer_did_show_question.append(typingTest)
